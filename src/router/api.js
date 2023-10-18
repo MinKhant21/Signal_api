5690a60ffc9ed6f5cdd635c4c590d7023e7c55f0
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-10-18 14:15:46 +0630

    myaccountAPi

--- src/router/api.js
+++ src/router/api.js
@@ -6,6 +6,8 @@ const apiRouter = express.Router();
 
 apiRouter.get('/getUser',verifyToken,ApiController.getUser)
 
+apiRouter.get('/myaccount',verifyToken,ApiController.myAccount)
+
 apiRouter.get('/join-chat-room',ApiController.joinChatRoom)
 
 module.exports = apiRouter
